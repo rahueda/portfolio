9e561898fdc1614bd943915a56c6b5f88d67970e
Rafael <rah.ueda@outlook.com>
2020-06-16 19:34:39 -0300

    Updates

---
+++ precache-manifest.0a423b707623a3ba70881cb38f6e3a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5819bd1a9d578997abf5cce86e4f1a8a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6dad6d25872b6be2c6f6",
+    "url": "/portfolio/static/css/main.ffee68f6.chunk.css"
+  },
+  {
+    "revision": "9ff0edb2cdcefe8cc170",
+    "url": "/portfolio/static/js/2.44fad725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.44fad725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dad6d25872b6be2c6f6",
+    "url": "/portfolio/static/js/main.81b66961.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "dc3eabcafb28befe23d803428b62feaf",
+    "url": "/portfolio/static/media/eu.dc3eabca.jpg"
+  }
+]);
